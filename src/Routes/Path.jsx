af1ce147fd84499bf50d96e0d4f80317376b7809
Aram Karapetyan <aramk007@gmail.com>
2024-05-26 15:28:23 -0600

    Cta section added

--- src/Routes/Path.jsx
+++ src/Routes/Path.jsx
@@ -4,6 +4,7 @@ import Navbar from "../Components/Common/Navbar/Navbar";
 import Hero from "../Components/Common/Hero/Hero";
 import Footer from "../Components/Common/Footer/Footer";
 import Skills from "../Components/Skills/Skills";
+import Cta from "../Components/CTA/Cta";
 // doing prop drilling to bring toggle props from app.jsx to path, then to navbar
 export default function Path({ isDark, toggleTheme }) {
   return (
@@ -16,6 +17,7 @@ export default function Path({ isDark, toggleTheme }) {
               <Navbar isDark={isDark} toggleTheme={toggleTheme} />
               <Hero />
               <Skills />
+              <Cta />
               <Footer />
             </>
           }
