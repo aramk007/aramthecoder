27d61141a13eaa29d7b5a1a319dc2e02eda9864b
Aram Karapetyan <aramk007@gmail.com>
2024-05-30 15:56:15 -0600

    added animation to transition mobile menu

--- src/Components/Common/Navbar/Navbar.jsx
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -4,102 +4,47 @@ import { Link } from "react-router-dom";
 import Switch from "react-switch";
 import { Icons, navbarIcons } from "../../../assets/Icons";
 import "./Navbar.css";
-// isDark prop is being imported from app.jsx for the toggle button / localStorage.
-// setIsDark will accept the dark/light theme state and send it back to app.jsx
-export default function Navbar({ isDark, toggleTheme }) {
-  const [checked, setChecked] = useState(false);
 
-  const [selected, setSelected] = useState(false);
+// Navbar component
+export default function Navbar({ isDark, toggleTheme }) {
+  const [selected, setSelected] = useState(false); // State to manage the hamburger menu toggle
+
+  // Function to handle opening and closing of the hamburger menu
+  const toggleMenu = () => {
+    setSelected(!selected);
+    // Trigger reflow to restart CSS animations
+    document
+      .querySelectorAll(".navItems, .viewWorkButton, .switch, .nav-footer")
+      .forEach((item) => {
+        item.classList.remove("fadeIn");
+        void item.offsetWidth; // Trigger reflow
+        item.classList.add("fadeIn");
+      });
+  };
 
   return (
     <>
-      <nav className="custom-hr navBar flex">
-        <img src={images.aramLogo} alt="logo" />
+      <nav className="navBar">
+        {/* Logo */}
+        <img src={images.aramLogo} alt="logo" className="logo" />
+        {/* Navigation links and theme switch */}
         <div className="side-nav-container">
           <div className="navLinks">
             <a href="#projects" className="navItems">
               Projects
             </a>
-            <Link className="navItems">Capabilities</Link>
-            <Link className="navItems">Contact</Link>
+            <Link to="/capabilities" className="navItems">
+              Capabilities
+            </Link>
+            <Link to="/contact" className="navItems">
+              Contact
+            </Link>
           </div>
           <div className="switch">
             <Switch
               checked={isDark}
               className="switch"
               checkedHandleIcon={
-                <>
-                  <div
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <img src={Icons.lightMode} width={20} height={20} />
-                  </div>
-                </>
-              }
-              uncheckedIcon={false}
-              checkedIcon={false}
-              uncheckedHandleIcon={
-                <>
-                  <div
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <img src={Icons.darkMode} width={20} height={20} />
-                  </div>
-                </>
-              }
-              onChange={toggleTheme}
-            />
-          </div>
-        </div>
-
-        <div className="hamburger-icon">
-          {!selected ? (
-            <>
-              <img
-                src={navbarIcons.menu}
-                alt="menu"
-                onClick={() => setSelected(!selected)}
-              />
-            </>
-          ) : (
-            <>
-              <img
-                src={navbarIcons.close}
-                alt="menu"
-                onClick={() => setSelected(!selected)}
-              />
-            </>
-          )}
-        </div>
-      </nav>
-
-      <div className={`hamburger-menu ${selected ? "show" : "hide"}`}>
-        <div className="navLinks-responsive">
-          <a href="#projects" className="navItems">
-            Projects
-          </a>
-          <Link className="navItems">Capabilities</Link>
-          <Link className="navItems">Contact</Link>
-          <button className="viewWorkButton">Contact Me</button>
-        </div>
-        <div className="switch">
-          <Switch
-            checked={isDark}
-            className="switch"
-            checkedHandleIcon={
-              <>
                 <div
                   style={{
                     width: "100%",
@@ -111,12 +56,10 @@ export default function Navbar({ isDark, toggleTheme }) {
                 >
                   <img src={Icons.lightMode} width={20} height={20} />
                 </div>
-              </>
-            }
-            uncheckedIcon={false}
-            checkedIcon={false}
-            uncheckedHandleIcon={
-              <>
+              }
+              uncheckedIcon={false}
+              checkedIcon={false}
+              uncheckedHandleIcon={
                 <div
                   style={{
                     width: "100%",
@@ -128,13 +71,69 @@ export default function Navbar({ isDark, toggleTheme }) {
                 >
                   <img src={Icons.darkMode} width={20} height={20} />
                 </div>
-              </>
+              }
+              onChange={toggleTheme}
+            />
+          </div>
+        </div>
+        {/* Hamburger icon for mobile view */}
+        <div className="hamburger-icon" onClick={toggleMenu}>
+          <img
+            src={selected ? navbarIcons.close : navbarIcons.menu}
+            alt="menu"
+          />
+        </div>
+      </nav>
+      {/* Hamburger menu content */}
+      <div className={`hamburger-menu ${selected ? "show" : "hide"}`}>
+        <div className="navLinks-responsive">
+          <a href="#projects" className="navItems fadeIn">
+            Projects
+          </a>
+          <Link to="/capabilities" className="navItems fadeIn">
+            Capabilities
+          </Link>
+          <Link to="/contact" className="navItems fadeIn">
+            Contact
+          </Link>
+          <button className="viewWorkButton fadeIn">Contact Me</button>
+        </div>
+        <div className="switch fadeIn">
+          <Switch
+            checked={isDark}
+            className="switch"
+            checkedHandleIcon={
+              <div
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img src={Icons.lightMode} width={20} height={20} />
+              </div>
+            }
+            uncheckedIcon={false}
+            checkedIcon={false}
+            uncheckedHandleIcon={
+              <div
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img src={Icons.darkMode} width={20} height={20} />
+              </div>
             }
             onChange={toggleTheme}
           />
         </div>
-
-        <div className="nav-footer">
+        <div className="nav-footer fadeIn">
           <p>
             I’m currently available for work
             <span className="pulse"></span>
