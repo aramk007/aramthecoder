416344f33d9260b0e6223aa4a116d367882d1abf
Aram Karapetyan <aramk007@gmail.com>
2024-05-30 16:34:05 -0600

    capitalization issue on image URL

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -3,7 +3,7 @@ export const images = {
   aramLightMode: new URL("./Aram-Light-Mode-Shadow-Pic.png", import.meta.url)
     .href,
   handEmoji: new URL("./handEmoji.png", import.meta.url).href,
-  aramDarkImage: new URL("./Aram-dark-image.png", import.meta.url).href,
+  aramDarkImage: new URL("./Aram-dark-Image.png", import.meta.url).href,
   cateringsList: new URL("./portfolio-cateringslist.png", import.meta.url).href,
   dealership: new URL("./portfolio-dealership.png", import.meta.url).href,
   kitchen: new URL("./portfolio-kitchen.png", import.meta.url).href,
