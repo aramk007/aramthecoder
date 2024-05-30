a1438daaadcb9af75b5706024737507322e22ab6
Aram Karapetyan <aramk007@gmail.com>
2024-05-29 20:24:59 -0600

    portfolio image updates, fontawesome, and more

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -3,4 +3,8 @@ export const images = {
   aramLightMode: "./src/assets/Images/Aram-Light-Mode-Shadow-Pic.png",
   handEmoji: "./src/assets/Images/handEmoji.png",
   aramDarkImage: "./src/assets/Images/Aram-dark-image.png",
+  cateringsList: "./src/assets/Images/portfolio-cateringslist.png",
+  dealership: "./src/assets/Images/portfolio-dealership.png",
+  kitchen: "./src/assets/Images/portfolio-kitchen.png",
+  restraunts: "./src/assets/Images/portfolio-restaurant.png",
 };
